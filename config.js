1160cd43cf6ec5029132208722501c30eca41193
Pemkaweb <134829796+Pemkaweb@users.noreply.github.com>
2026-03-30 17:51:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,16 @@
         // BACKEND_URL: 'https://watermelon.boto.education/scorm/test',
 
         // Bot avatar override (uncomment one to use)
-        BOT_AVATAR: './assets/Marina_av.jpg', // default
+        BOT_AVATAR: './assets/avatar.jpg', // default
         // BOT_AVATAR: './assets/vyacheslav_avatar.png',
         // BOT_AVATAR: './assets/darina_avatar.png',
 
+        // UI flags
+        SHOW_INTRO_CARD: false, // Показывать вводную карточку перед кнопкой старта
+        DEEP_LINK_TOKEN: "",
+
         // SCORM Parameters
-        DEBUG: false,
+        DEBUG: true,
         SCORM_VERSION: '1.2',    // '1.2' for SCORM 1.2, '2004' for SCORM 2004 (3rd/4th Ed.)
         PASSING_SCORE: 70,
         MAX_SCORE: 100,
